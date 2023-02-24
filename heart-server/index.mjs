1da991d29b62cb33701f15bc2ce938b50c27a992
Insaneboy9 <daryl_seah89@hotmail.com>
2023-02-25 00:15:11 +0800

    progress for posting insert

--- heart-server/index.mjs
+++ heart-server/index.mjs
@@ -1,20 +1,35 @@
 import "./loadEnvironment.mjs";
 import db from "./mongoconn.mjs";
-import express from 'express';
+
+import express from "express";
 const app = express();
 const port = 8080;
 
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-app.get('/fullData', async (req, res) => {
-  const patientsCollection = db.collection('patients');
+app.get("/fullData", async (req, res) => {
+  const patientsCollection = db.collection("patients");
   const patientsDocuments = await patientsCollection.find().toArray();
-  res.json(patientsDocuments);
+  const recordsCollection = db.collection("medicalRecords");
+  const recordsDocuments = await recordsCollection.find().toArray();
+  const fullData = patientsDocuments.concat(recordsDocuments);
+  res.json(fullData);
 });
 
+// Handle POST request
+app.post("/insertEntry", (req, res) => {
+  // Retrieve data from request body
+  const data = req.body;
+  // Separate the "sex" and "age" values into a new object
+  const patientData = (({  age, sex, target }) => ({age, sex, target }))(data);
+  // Separate the other values into a new object
+  const recordData = (({ exang, ca, cp, thalach, fbs, rest_ecg, chol,trtbps }) => ({ exang, ca, cp, thalach, fbs, rest_ecg, chol,trtbps }))(data);
+
+  // Send response
+  res.send("Data received");
+});
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
